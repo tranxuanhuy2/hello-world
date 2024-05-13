2af8bda698ec15066df5528b05df50fce07e6fa3
Huy Tran <tranxuanhuy4265@gmail.com>
2024-05-13 20:49:05 +0700

    init project

---
+++ index.js
@@ -0,0 +1,19 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const jsonParser = bodyParser.json();
+
+app.get('/ping', (req, res) => {
+  return res.send('pong');
+});
+
+app.get('/hello-world', (req, res) => {
+  return res.send('hello-world');
+});
+
+const port = process.env.PORT || 8040;
+
+app.listen(port, () => {
+  console.log(`Server is running in port ${port}.`)
+})
